5ad1703d4c471a4e34fe49117ece24f0c362efb7
altramax <ajayi_ezekiel@yahoo.com>
2023-02-10 12:44:38 +0100

    update added

--- index.js
+++ index.js
@@ -5,26 +5,24 @@ const deleteBtn = document.getElementById('deletebtn');
 const notes = document.getElementById('noteArea');
 const noteBody = document.getElementById('note-body');
 const noteTitle = document.getElementById('noteTitle');
-const color = document.getElementById('color');
-const mark = document.getElementById('mark')
 
 let g = 0;
-
 // add new note 
 addBtn.addEventListener('click', function(){
-    let alt = title.value.slice(0,30) + "..."
+    let alt = title.value.slice(0,50)
     
      if(!title.value){
        let newTitle = body.value;
-       alt = newTitle.slice(0,20) + "..."
+       alt = newTitle.slice(0,40) + "..."
      }
 
     let input = `
     <div class="notes">
        <p id="noteTitle" class="note-title" onclick ="noteDisplay(${++g})">${alt}</p> 
-       <p id="note-body${g}" class=" hidden">${body.value}</p>
-       <p class="deletebtn" onclick ="noteDelete(${g})">❌</p>
-       <p class="delete"> DELETE </p>
+       <p id="note-body${g}" class="note-body">${body.value}
+       <span class="delete"onclick ="noteDelete(${g})">DELETE NOTE</span>
+      </p>
+       
     </div>
     `
     if(body.value){
@@ -36,23 +34,10 @@ addBtn.addEventListener('click', function(){
 
 // open and close note 
 function noteDisplay(e){ 
-    document.getElementById('note-body' + e).classList.toggle('hidden')
+    document.getElementById('note-body' + e).classList.toggle('display')
 }
 
 //  Delete note
 function noteDelete(e){
     document.getElementById('note-body' + e).parentNode.style.display="none"
 }
-
-
-{/* <div class="markgroup">
-            <input id="color" type="color" class="color">
-            <span class="mark" onclick="colorF(g)">✔</span>
-        </div> */}
-
-
-// function colorF(e){ 
-//     document.getElementById('note-body' + e).parentNode.style.backgroundColor =
-//      document.getElementById('color').value;
-// }
-
