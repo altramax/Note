864c7322e268b3ed56d89cf79631d934b0275bd5
altramax <ajayi_ezekiel@yahoo.com>
2023-02-10 13:53:39 +0100

    Updated The JavaScript

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const notes = document.getElementById('noteArea');
 const noteBody = document.getElementById('note-body');
 const noteTitle = document.getElementById('noteTitle');
 
-let g = 0;
+let count = 0;
 // add new note 
 addBtn.addEventListener('click', function(){
     let alt = title.value.slice(0,50)
@@ -18,9 +18,9 @@ addBtn.addEventListener('click', function(){
 
     let input = `
     <div class="notes">
-       <p id="noteTitle" class="note-title" onclick ="noteDisplay(${++g})">${alt}</p> 
-       <p id="note-body${g}" class="note-body">${body.value}
-       <span class="delete"onclick ="noteDelete(${g})">DELETE NOTE</span>
+       <p id="noteTitle" class="note-title" onclick ="noteDisplay(${++count})">${alt}</p> 
+       <p id="note-body${count}" class="note-body">${body.value}
+       <span class="delete"onclick ="noteDelete(${count})">DELETE NOTE</span>
       </p>
        
     </div>
