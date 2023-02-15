831d4878d0aef5a6226246be47e9ca5127d1b950
Ezekiel Ajayi <ajayiezekiel559@gmail.com>
2023-02-15 21:38:42 +0100

    Style Fixes

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-"use strict"
-const addBtn = document.getElementById('add');
-let title = document.getElementById('title');
-let body = document.getElementById('body');
-const deleteBtn = document.getElementById('deletebtn');
-const notes = document.getElementById('noteArea');
-const noteBody = document.getElementById('note-body');
-const noteTitle = document.getElementById('noteTitle');
+"use strict";
+const addBtn = document.getElementById("add");
+let title = document.getElementById("title");
+let body = document.getElementById("body");
+const deleteBtn = document.getElementById("deletebtn");
+const notes = document.getElementById("noteArea");
+const noteBody = document.getElementById("note-body");
+const noteTitle = document.getElementById("noteTitle");
 
 let count = 0;
-let dropDown  = true;
-
+let dropDown = true;
 
 // ADD NEW NOTE
-addBtn.addEventListener('click', function(){
+addBtn.addEventListener("click", function () {
+  let alt = title.value.slice(0, 100);
 
-let alt = title.value.slice(0,50)
-
-  if(!title.value){
+  if (!title.value) {
     let newTitle = body.value;
-    alt = newTitle.slice(0,40) + "..."
+    alt = newTitle.split(" ").slice(0, 5).join(" ");
   }
 
-`${++count}`
+  `${++count}`;
 
-let input = `
+  let input = `
 
 <div id="notes${count}"  data="true" class="notes">
  <div id="forChange${count}">
@@ -36,64 +34,64 @@ let input = `
     <span id="Delete${count}" class="delete" onclick ="deleteNote(${count})">DELETE NOTE</span>
   </div>
 </div>
-`
-    notes.insertAdjacentHTML("afterbegin", input) 
- return  body.value = title.value = ""
-})
+`;
 
+  if (alt || body.value) {
+    notes.insertAdjacentHTML("afterbegin", input);
+  }
+
+  return (body.value = title.value = "");
+});
 
 // OPEN AND CLOSE NOTE
-function displayNote(e){ 
-  if(dropDown){
-    document.getElementById('notebody' + e).classList.toggle('display')
-    document.getElementById('Edit' + e).classList.toggle('display')
-    document.getElementById('Delete' + e).classList.toggle('display')}
+function displayNote(e) {
+  if (dropDown) {
+    document.getElementById("notebody" + e).classList.toggle("display");
+    document.getElementById("Edit" + e).classList.toggle("display");
+    document.getElementById("Delete" + e).classList.toggle("display");
+  }
 }
-
 
 //  DELETE NOTE
-    function deleteNote(e){
-    dropDown = true;
-    document.getElementById('forChange' + e).parentNode.style.display="none"
+function deleteNote(e) {
+  dropDown = true;
+  document.getElementById("forChange" + e).parentNode.style.display = "none";
 }
-
 
 // EDIT NOTE
-  function editNote(e){
+function editNote(e) {
+  // stop dropdown display
+  dropDown = false;
 
-  // stop dropdown display 
-   dropDown = false
+  // toggle button
+  document.getElementById("Edit" + e).classList.toggle("hidden");
+  document.getElementById("updateNote" + e).classList.toggle("hidden");
 
-// toggle button 
-  document.getElementById('Edit' + e).classList.toggle('hidden')
-  document.getElementById('updateNote' + e).classList.toggle('hidden')
+  // change text area
+  let currentBodyText = document.getElementById("notebody" + e).innerHTML;
+  let currentHeadText = document.querySelector(".num" + e).innerHTML;
 
-  // change text area 
-  let currentBodyText = document.getElementById('notebody' + e).innerHTML;
-  let currentHeadText = document.querySelector('.num' + e).innerHTML
- 
-  let noteTitle = document.querySelector('.num' + e)
-  let noteBody = document.getElementById('notebody' + e)
-  
+  let noteTitle = document.querySelector(".num" + e);
+  let noteBody = document.getElementById("notebody" + e);
+
   noteTitle.innerHTML = `<textarea id="titleEdit${e}" name="title" class="titleEdit" cols="35" rows="2">${currentHeadText}
-  </textarea>`
+  </textarea>`;
   noteBody.innerHTML = `<textarea id="bodyEdit${e}" name="body" class="bodyEdit" cols="35" rows="6">${currentBodyText}
-  </textarea>`
+  </textarea>`;
 }
 
+// Update edited note
+function updateNote(e) {
+  let newNote = document.getElementById("notes" + e);
+  let editedTitle = document.getElementById("titleEdit" + e).value;
+  let editedBody = document.getElementById("bodyEdit" + e).value;
+  let alt = editedTitle.slice(0, 100);
 
-// Update edited note 
-function updateNote(e){
-  let newNote = document.getElementById('notes' + e)
-  let editedTitle = document.getElementById('titleEdit' + e).value;
-  let editedBody = document.getElementById('bodyEdit' + e).value;
-  let alt = editedTitle.slice(0,50)
-  
-    if(!editedTitle){
-      let newTitle = editedBody.value;
-      alt = newTitle.slice(0,40) + "..."
-    }
-    
+  if (!editedTitle) {
+    let newTitle = editedBody.value;
+    alt = newTitle.split(" ").slice(0, 5).join(" ");
+  }
+
   let input = `
    <div id="forChange${e}">
       <p id="noteTitle${e}" class="note-title num${e}" onclick ="displayNote(${e})">${alt}</p> 
@@ -104,7 +102,7 @@ function updateNote(e){
       </div>
       <span id="Delete${e}" class="delete" onclick ="deleteNote(${e})">DELETE NOTE</span>
     <div>
-    `
-    dropDown = true;
-    return newNote.innerHTML = input;
+    `;
+  dropDown = true;
+  return (newNote.innerHTML = input);
 }
